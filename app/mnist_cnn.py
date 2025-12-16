cbaf4a1e318ea8c541de3da6db187431bd1c471e
l-reich <64188912+l-reich@users.noreply.github.com>
2025-12-16 17:35:07 +0100

    cnn class def in app/mnist_cnn

---
+++ app/mnist_cnn.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(64 * 7 * 7, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(-1, 64 * 7 * 7)
+        x = self.fc(x)
+        return x
